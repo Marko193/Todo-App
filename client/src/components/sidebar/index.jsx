f001df06e862c029ef8d034e83762a3cd2a30dcd
Mark Pavlenko <marko.pavlenko@nure.ua>
2023-04-18 22:20:04 +0300

    feat: add sidebar

--- client/src/components/sidebar/index.jsx
+++ client/src/components/sidebar/index.jsx
@@ -1,5 +1,12 @@
+import './styles.css';
 export default function Sidebar() {
     return (
-        <>Sidebar content</>
+        <div className='sidebar'>
+            <div className='sidebar_items'>
+            <span className='item'>Мои задачи</span>
+            <span className='item'>Выполенные</span>
+            <span className='item'>Важные</span>
+            </div>
+        </div>
     )
 }
